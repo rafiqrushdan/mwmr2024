90ab6d4b4169d32ea6187ccff035363baf2f81ec
rafiqrushdan <rafiqkizars21@gmail.com>
2024-03-30 07:46:06 +0000

    Big Update

--- README.md
+++ README.md
@@ -28,13 +28,42 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 
 
 
-If you want to install the VS Code in the **Raspberry Pi** , run the command below:
+If you want to install the VS Code in the **Raspberry Pi**, run the command below:
 
 
 `````
 curl -L https://aka.ms/linux-arm64-deb > code_arm64.deb
 sudo apt install ./code_arm64.deb
 ``````
+
+## Hardware Configuration
+
+All the pins in connected with **EPS32 DevKit V1 38 Pins** please refer to the picture below:
+
+![Lower Control](fyp2_schematic_diagram.drawio.png)
+
+
+### Motor Pins Configuration
+
+This project use  [IG42e-24K](https://my.cytron.io/p-12v-248rpm-10kgfcm-planetary-dc-geared-motor-with-encoder) motor with the specs of 12V 248RPM 10kgfcm and 120 ppr for the encoder. For the driver we use the [BTS7960](https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.handsontec.com/dataspecs/module/BTS7960%2520Motor%2520Driver.pdf&ved=2ahUKEwiyi_33vpaFAxUm2DgGHY61DuQQFnoECCAQAQ&usg=AOvVaw0IWVR-1gosuocPG1Wmpnxk).
+
+Motor | RPWM | LPWM | Encoder 
+:----------: | :----: | :----------:| :----------: 
+| Motor 1 |12 |13 |15,2
+| Motor 2 |27|28 |0,4
+| Motor 3 |33 |25 |16,17
+| Motor 4 |32 |35 |5,18
+
+*Connect all the **L_EN** and **R_EN** pin with the **5V pins** to the microcontroller.*
+
+### IMU Pins Configuration
+This project uses [ICM 20948](https://www.adafruit.com/product/4554) because the library is more fully established and cheaper than the other IMUs eg; MPU 9250, and MPU 6050. Some of the MPUs have fake magnetometers and inaccurate readings.
+
+-SCL = pin 22
+-SDA = pin 21
+
+
+
 
 
 
--- hardware/firmware/src/main.cpp
+++ hardware/firmware/src/main.cpp
@@ -2,6 +2,7 @@
 
 #include <ros.h>
 #include <std_msgs/Int16.h>
+#include <std_msgs/Float32.h>
 
 #include "motor.h"
 #include "encoder.h"
@@ -9,6 +10,27 @@
 #include "config.h"
 
 ros::NodeHandle nh;
+
+  /////////////PID///////////////
+  #include <PID_v1.h>
+  //Define Variables we'll be connecting to
+double Setpoint, Input, Output;
+double Setpoint1, Input1, Output1;
+
+//Specify the links and initial tuning parameters
+double Kp1=2, Ki1=0, Kd1=0;
+double Kp2=2, Ki2=5, Kd2=1;
+double Kp3=32, Ki3=5, Kd3=1;
+double Kp4=2, Ki4=5, Kd4=1;
+
+PID motor1_PID(&Input1, &Output1, &Setpoint1, Kp1, Ki1, Kd1, DIRECT);
+PID motor2_PID(&Input, &Output, &Setpoint, Kp2, Ki2, Kd2, DIRECT);
+PID motor3_PID(&Input, &Output, &Setpoint, Kp3, Ki3, Kd3, DIRECT);
+PID motor4_PID(&Input, &Output, &Setpoint, Kp4, Ki4, Kd4, DIRECT);
+
+
+
+
 
 /////////////// MOTOR ///////////////
 Motor motor1(MOTOR1A, MOTOR1B);
@@ -27,15 +49,15 @@ ros::Subscriber<std_msgs::Int16> sub3("motor3", &motor3_callback);
 ros::Subscriber<std_msgs::Int16> sub4("motor4", &motor4_callback);
 
 ////////////// ENCODER //////////////
-Encoder encoder1(ENC1A, ENC1B, CPR, true);
+Encoder encoder1(ENC1A, ENC1B, CPR, false);
 Encoder encoder2(ENC2A, ENC2B, CPR, false);
 Encoder encoder3(ENC3A, ENC3B, CPR, false);
 Encoder encoder4(ENC4A, ENC4B, CPR, false);
 
-std_msgs::Int16 enc1;
-std_msgs::Int16 enc2;
-std_msgs::Int16 enc3;
-std_msgs::Int16 enc4;
+std_msgs::Float32 enc1;
+std_msgs::Float32 enc2;
+std_msgs::Float32 enc3;
+std_msgs::Float32 enc4;
 
 ros::Publisher pub1("enc1", &enc1);
 ros::Publisher pub2("enc2", &enc2);
@@ -68,15 +90,25 @@ void setup()
 
   attachInterrupt(digitalPinToInterrupt(ENC1A), encoder1Update, CHANGE);
   attachInterrupt(digitalPinToInterrupt(ENC1B), encoder1Update, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(ENC2A), encoder1Update, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(ENC2B), encoder1Update, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(ENC3A), encoder1Update, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(ENC3B), encoder1Update, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(ENC4A), encoder1Update, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(ENC4B), encoder1Update, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENC2A), encoder2Update, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENC2B), encoder2Update, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENC3A), encoder3Update, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENC3B), encoder3Update, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENC4A), encoder4Update, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENC4B), encoder4Update, CHANGE);
 
   prev_time = millis();
+
+Setpoint=0;
+
+  //turn the PID on
+  motor1_PID.SetMode(AUTOMATIC);
+  motor2_PID.SetMode(AUTOMATIC);
+  motor3_PID.SetMode(AUTOMATIC);
+  motor4_PID.SetMode(AUTOMATIC);
+
 }
+
 
 void loop()
 {
@@ -87,14 +119,25 @@ void loop()
     prev_time = millis();
   }
   delay(20);
+
+  //Input=encoder1.get_rpm();
+  //Setpoint=speed.data;
+  //motor1_PID.Compute();
+
+  //analogWrite(MOTOR1A, Output);
+
+
+  Input1=encoder1.get_rpm();
+  motor1_PID.Compute();
+  motor1.setSpeed(Output1);
 }
 
 void send_encoder_data()
 {
-  enc1.data = encoder1.get_count();
-  enc2.data = encoder2.get_count();
-  enc3.data = encoder3.get_count();
-  enc4.data = encoder4.get_count();
+  enc1.data = encoder1.get_rpm();
+  enc2.data = encoder2.get_rpm();
+  enc3.data = encoder3.get_rpm();
+  enc4.data = encoder4.get_rpm();
   pub1.publish(&enc1);
   pub2.publish(&enc2);
   pub3.publish(&enc3);
@@ -103,22 +146,33 @@ void send_encoder_data()
 
 void motor1_callback(const std_msgs::Int16 &speed)
 {
-  motor1.setSpeed(speed.data);
+  Setpoint1=speed.data;
 }
 
 void motor2_callback(const std_msgs::Int16 &speed)
 {
-  motor2.setSpeed(speed.data);
+  Setpoint=speed.data;
+  Input=encoder2.get_rpm();
+  motor2_PID.Compute();
+  motor2.setSpeed(Output);
 }
 
 void motor3_callback(const std_msgs::Int16 &speed)
 {
-  motor3.setSpeed(speed.data);
+  Setpoint=speed.data;
+  Input=encoder3.get_rpm();
+  motor3_PID.Compute();
+  motor3.setSpeed(Output);
+  
 }
 
 void motor4_callback(const std_msgs::Int16 &speed)
 {
-  motor4.setSpeed(speed.data);
+
+  Setpoint=speed.data;
+  Input=encoder4.get_rpm();
+  motor4_PID.Compute();
+  motor4.setSpeed(Output);
 }
 
 void encoder1Update()
@@ -140,3 +194,4 @@ void encoder4Update()
 {
   encoder4.encoderUpdate();
 }
+
