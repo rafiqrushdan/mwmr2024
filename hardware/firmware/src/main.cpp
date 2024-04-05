4518e36a7ce58abbec49da71938eddea2545dedb
rafiqrushdan <rafiqkizars21@gmail.com>
2024-04-05 09:25:59 +0800

    Update

--- hardware/firmware/src/main.cpp
+++ hardware/firmware/src/main.cpp
@@ -3,6 +3,7 @@
 #include <ros.h>
 #include <std_msgs/Int16.h>
 #include <std_msgs/Float32.h>
+#include <Adafruit_Sensor.h>
 
 #include "motor.h"
 #include "encoder.h"
@@ -12,8 +13,13 @@
 #include <geometry_msgs/Pose.h>
 #include <sensor_msgs/Imu.h>
 
+// #include <Arduino-ICM20948.h>
+#include <Adafruit_ICM20X.h>
+#include <Adafruit_ICM20948.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
 ros::NodeHandle nh;
-// ros::init(argc, argv, "controller_node");
 
 /////////////PID///////////////
 #include <PID_v1.h>
@@ -67,12 +73,13 @@ ros::Publisher pub2("enc2", &enc2);
 ros::Publisher pub3("enc3", &enc3);
 ros::Publisher pub4("enc4", &enc4);
 
-
 ////////// IMU ///////////
 Adafruit_ICM20948 icm20948;
+
+
+
 sensor_msgs::Imu imu_data;
 ros::Publisher pub5("/imu/data", &imu_data);
-
 
 // declare the function protoype
 void encoder1Update();
@@ -81,9 +88,8 @@ void encoder3Update();
 void encoder4Update();
 void send_imu_data();
 
-
-//void PIDcallback();
-//void computePID();
+// void PIDcallback();
+// void computePID();
 
 void send_encoder_data();
 unsigned long prev_time;
@@ -102,7 +108,7 @@ void setup()
   nh.advertise(pub3);
   nh.advertise(pub4);
 
-  //PIDcallback();
+  // PIDcallback();
 
   delay(100);
 
@@ -117,13 +123,16 @@ void setup()
 
   prev_time = millis();
 
-  //Setpoint1=0;
+  // Setpoint1=0;
 
   // turn the PID on
   motor1_PID.SetMode(AUTOMATIC);
   motor2_PID.SetMode(AUTOMATIC);
   motor3_PID.SetMode(AUTOMATIC);
   motor4_PID.SetMode(AUTOMATIC);
+
+
+
 }
 
 void loop()
@@ -154,7 +163,7 @@ void loop()
   motor3_PID.Compute();
   motor3.setSpeed(Output3);
 
-  Input4 =enc4.data;
+  Input4 = enc4.data;
   motor4_PID.Compute();
   motor4.setSpeed(Output4);
 
@@ -214,32 +223,73 @@ void encoder4Update()
   encoder4.encoderUpdate();
 }
 
-
 void send_imu_data()
 {
-    sensors_event_t accel, gyro, mag;
-    icm20948.getEvent(&accel, &gyro, &mag);
+  sensors_event_t accel, gyro, mag;
+  icm20948.getEvent(&accel, &gyro, &mag);
 
-    imu::Vector<3> acc;
-    acc.x() = accel.acceleration.x;
-    acc.y() = accel.acceleration.y;
-    acc.z() = accel.acceleration.z;
+  // imu::Vector<3> acc;
+  // acc.x() = accel.acceleration.x;
+  // acc.y() = accel.acceleration.y;
+  // acc.z() = accel.acceleration.z;
 
-    imu::Quaternion quat;
-    quat.w() = 1.0; // Default value for the quaternion's scalar component
-    quat.x() = gyro.gyro.x;
-    quat.y() = gyro.gyro.y;
-    quat.z() = gyro.gyro.z;
+  // imu::Quaternion quat;
+  // quat.w() = 1.0; // Default value for the quaternion's scalar component
+  // quat.x() = gyro.gyro.x;
+  // quat.y() = gyro.gyro.y;
+  // quat.z() = gyro.gyro.z;
 
-    imu_data.linear_acceleration.x = acc.x();
-    imu_data.linear_acceleration.y = acc.y();
-    imu_data.linear_acceleration.z = acc.z();
-    imu_data.orientation.w = quat.w();
-    imu_data.orientation.x = quat.x();
-    imu_data.orientation.y = quat.y();
-    imu_data.orientation.z = quat.z();
+  // imu_data.linear_acceleration.x = acc.x();
+  // imu_data.linear_acceleration.y = acc.y();
+  // imu_data.linear_acceleration.z = acc.z();
+  // imu_data.orientation.w = quat.w();
+  // imu_data.orientation.x = quat.x();
+  // imu_data.orientation.y = quat.y();
+  // imu_data.orientation.z = quat.z();
 
-    pub1.publish(&imu_data);
+    // Calculate quaternion
+  float ax = accel.acceleration.x;
+  float ay = accel.acceleration.y;
+  float az = accel.acceleration.z;
+  float gx = gyro.gyro.x;
+  float gy = gyro.gyro.y;
+  float gz = gyro.gyro.z;
+  float qw = sqrt(1 + ax*ax + ay*ay + az*az) / 2.0;
+  float qx = (gy * az - gz * ay) / (4 * qw);
+  float qy = (gz * ax - gx * az) / (4 * qw);
+  float qz = (gx * ay - gy * ax) / (4 * qw);
+
+  // Publish quaternion
+  imu_data.orientation.w = qw;
+  imu_data.orientation.x = qx;
+  imu_data.orientation.y = qy;
+  imu_data.orientation.z = qz;
+
+  // Publish linear acceleration
+  imu_data.linear_acceleration.x = accel.acceleration.x;
+  imu_data.linear_acceleration.y = accel.acceleration.y;
+  imu_data.linear_acceleration.z = accel.acceleration.z;
+
+  // icm20948.task();
+  // float quat_w, quat_x, quat_y, quat_z;
+  // if (icm20948.quat6DataIsReady())
+  // {
+  //   icm20948.readQuat6Data(&quat_w, &quat_x, &quat_y, &quat_z);
+  // }
+
+  // // Read acceleration data
+  // float accel_x, accel_y, accel_z;
+  // if (icm20948.accelDataIsReady())
+  // {
+  //   icm20948.readAccelData(&accel_x, &accel_y, &accel_z);
+  // }
+  // imu_data.linear_acceleration.x = accel_x;
+  // imu_data.linear_acceleration.y = accel_y;
+  // imu_data.linear_acceleration.z = accel_z;
+  // imu_data.orientation.w = quat_w;
+  // imu_data.orientation.x = quat_x;
+  // imu_data.orientation.y = quat_y;
+  // imu_data.orientation.z = quat_z;
+
+  pub5.publish(&imu_data);
 }
-
-
